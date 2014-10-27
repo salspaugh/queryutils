a1a3d9905fbc82776f5bce3c798dc3b8b0a379db
jaytlin <heyjaylin@gmail.com>
2014-10-27 14:21:56 -0700

    Edit README part 3 of setting up.

--- scripts/load_base_postgres.sh
+++ scripts/load_base_postgres.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-python scripts/loaddb.py -s csvfiles -d postgresdb -p test/data/diag2014.csv -u splqueryutils_test_postgres -w splqueryutils_test_postgres -b splqueryutils_test_postgres -v diag_2014
+python scripts/loaddb.py -s csvfiles -d postgresdb -p test/data/format2014.csv -u splqueryutils_test_postgres -w splqueryutils_test_postgres -b splqueryutils_test_postgres -v format_2014
